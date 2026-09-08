bac8ec6134049b28c5661b2ff86255873e84cd9e
Raghav Mehta <152819089+1RaghavM@users.noreply.github.com>
2026-09-08 14:19:04 -0400

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        check = {}
+        
+        for num in nums:
+            if num not in check:
+                check[num] = 1
+            else:
+                check[num] += 1
+        
+        for key, value in check.items():
+            if value > len(nums) / 2:
+                return key
